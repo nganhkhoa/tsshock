0e2407a2b8136ae5a5cbca7048273b0ef6b748f6
nganhkhoa <mail.nganhkhoa@gmail.com>
2023-01-05 15:42:16 +0700

    [gpu-bruteforce] update new p,q,h1,h2 generation code

--- bruteforce/crack.py
+++ bruteforce/crack.py
@@ -9,6 +9,23 @@ from typing import List, Tuple
 import bisect
 import random
 import time
+SMALL_PRIMES = list(primerange(10 ** 3, 10 ** 4))
+SECP256K1_Q = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+def search_prime(min_p, max_p) -> int:
+    """Find a prime `p` such that Zp* is of smooth order."""
+    lb = min_p // SMALL_PRIMES[-1] + 1
+    ub = max_p // SMALL_PRIMES[0]
+    while True:
+        p = 2
+        while p < lb:
+            p *= random.choice(SMALL_PRIMES)
+        if p > ub:
+            continue
+        p *= SMALL_PRIMES[bisect.bisect_right(SMALL_PRIMES, min_p // p)]
+        p += 1
+        if isprime(p):
+            return p
 
 def _hash_ints(ints: List[int], log=False) -> int:
     hash_obj = SHA512.new(truncate='256')
@@ -39,8 +56,10 @@ def _int_buffer(x):
     return x.to_bytes((x.bit_length() + 7) // 8, "big")
 
 
-P_LENGTH = 26
-Q_LENGTH = 2048 - P_LENGTH
+
+N_LENGTH = 2048
+P_LENGTH = 16
+Q_LENGTH = N_LENGTH - P_LENGTH
 SALT = int.from_bytes(b"ING TS dlog proof sub-protocol v1.0", "big")
 
 
@@ -51,40 +70,37 @@ def _gen_malicious_params():
     mask_p = (0b11 << (P_LENGTH - 2)) | 1
     mask_q = (0b11 << (Q_LENGTH - 2)) | 1
 
-    # p = q = 0
-    # while not isPrime(p):
-    #     p = random.getrandbits(P_LENGTH) | mask_p
-    # while not isPrime(q):
-    #     q = random.getrandbits(Q_LENGTH) | mask_q
-    p = 59399
-    q = 272379560429502703646661135106056719719067080873162961823827215490061256422819659552373344951825546324458350620082031068529166308796854290135714817531606786573945775017894758654415070807593192678792937663299335046565532465330588833545771929405703217024587583716414780059599703421685510408057011889147073939499940948444788337443830285948044506503933809432637228751503995389916587635620395123173627949922582932614470156074759502701386357997787786757457957185361386462053568644592955219702419606232499979556498045137552432784518549944634447170594301216051839604055287716430710964545394442374937196964761786349154823
+    print("generating p, q")
+    p = q = 0
+    while not isPrime(p):
+        p = random.getrandbits(P_LENGTH) | mask_p
+    while not (isprime(q) and gcd(p - 1, q - 1) == 2 and is_quad_residue(2, q)):
+        q = search_prime(2 ** (N_LENGTH - 1) // p + 1, 2 ** N_LENGTH // p - 1)
+
     N = p * q
     phiN = (p - 1) * (q - 1)
     order = phiN // 4
 
-    # search for h1, h2
-    # h1 = 2
-    # h2 = int(crt([p, q], [1, 2])[0])
-    # while True:
-    #     try:
-    #         assert is_quad_residue(h1, p) and is_quad_residue(h1, q), "inappropriate h1"
-    #         secret_q = discrete_log(q, h2, h1)
-    #         assert pow(h1, secret_q, q) == h2 % q, "log(h2, base=h1, modulo=q) does not exist"
-    #         secret = int(crt([(p - 1) // 2, (q - 1) // 2], [0, secret_q])[0])
-    #         k = secret // ((p - 1) // 2)
-    #         assert gcd(k, order) == 1, "k has no inverse"
-    #         k_inv = pow(k, -1, order)
-    #     except Exception as err:
-    #         # print(err)
-    #         h1 += 1
-    #         continue
-    #     break
-
-    h1 = 3
-    h2 = 13760615392898476588229320545557985480207268925712192831339750926557894674480849200585901386966226600311635873326544209582093481920417078737656312581696774857715740553904043207221049377199608094132619210749882406552490700148501347870732397873576126524082164729353274688610977016863551985815040240639710175423537016715430706807662306046095208468578736052536832796525981847098586007351542361622731684030088889755683032284896850076474038806048238986986775997004457244062946287924836097699366238506865898967194281240349148904273877143202932271058424097434938936796873135434079517928833327228781827190659765446359301657962
-    print("h1", h1)
-    print("h2", h2)
-
+    print("searching for h1, h2")
+    h1 = 2
+    h2 = int(crt([p, q], [1, 2])[0])
+    while True:
+        try:
+            assert is_quad_residue(h1, p) and is_quad_residue(h1, q), "inappropriate h1"
+            secret_q = discrete_log(q, h2, h1)
+            assert pow(h1, secret_q, q) == h2 % q, "log(h2, base=h1, modulo=q) does not exist"
+            secret = int(crt([(p - 1) // 2, (q - 1) // 2], [0, secret_q])[0])
+            k = secret // ((p - 1) // 2)
+            assert gcd(k, order) == 1, "k has no inverse"
+            k_inv = pow(k, -1, order)
+        except Exception as err:
+            print(err)
+            h1 += 1
+            continue
+        break
+    print("done searching for h1, h2")
+    assert pow(h1, order, N) == 1
+    assert pow(h2, order, N) == 1
 
     # build proof for log(h2, base=h1)
     # g, V = h2, h1
