074800faa61ce538d2f5d33128297bfc56579be4
nganhkhoa <mail.nganhkhoa@gmail.com>
2023-01-11 12:17:23 +0700

    [gpu-bruteforce] add support for cli arguments

--- bruteforce/crack.py
+++ bruteforce/crack.py
@@ -13,6 +13,8 @@ import time
 import os
 import subprocess
 
+import argparse
+import sys
 
 from cuda_run import work
 
@@ -112,26 +114,43 @@ def modify_template(template: str, ints: List[int]) -> str:
 
     return template
 
-
-N_LENGTH = 2048
-P_LENGTH = 16
-Q_LENGTH = N_LENGTH - P_LENGTH
 SALT = int.from_bytes(b"ING TS dlog proof sub-protocol v1.0", "big")
 
-
-def _gen_malicious_params():
+def _gen_malicious_params(args):
     """Search for suitable p, q, h1, h2."""
+
+    P_LENGTH = args.Pbit
+    if args.p is not None:
+        P_LENGTH = len(bin(args.p)) - 2
+    if args.p is not None:
+        Q_LENGTH = len(bin(args.q)) - 2
+
+    N_LENGTH = args.Nbit
+    assert N_LENGTH > P_LENGTH, "N is smaller than P"
+    Q_LENGTH = N_LENGTH - P_LENGTH
 
     # search for p, q
     mask_p = (0b11 << (P_LENGTH - 2)) | 1
     mask_q = (0b11 << (Q_LENGTH - 2)) | 1
 
     print("generating p, q")
-    p = q = 0
-    while not isPrime(p):
-        p = random.getrandbits(P_LENGTH) | mask_p
-    while not (isprime(q) and gcd(p - 1, q - 1) == 2 and is_quad_residue(2, q)):
-        q = search_prime(2 ** (N_LENGTH - 1) // p + 1, 2 ** N_LENGTH // p - 1)
+
+    if args.p is not None:
+        p = args.p
+        assert isPrime(p), "p provided is not prime"
+    else:
+        p = 0
+        while not isPrime(p):
+            p = random.getrandbits(P_LENGTH) | mask_p
+
+    if args.q is not None:
+        q = args.q
+        assert isPrime(q), "q provided is not prime"
+    else:
+        q = 0
+        while not (isprime(q) and gcd(p - 1, q - 1) == 2 and is_quad_residue(2, q)):
+            q = search_prime(2 ** (N_LENGTH - 1) // p + 1, 2 ** N_LENGTH // p - 1)
+
 
     N = p * q
     phiN = (p - 1) * (q - 1)
@@ -150,14 +169,13 @@ def _gen_malicious_params():
             assert gcd(k, order) == 1, "k has no inverse"
             k_inv = pow(k, -1, order)
         except Exception as err:
-            print(err)
+            # print(err)
             h1 += 1
             continue
         break
     print("done searching for h1, h2")
     assert pow(h1, order, N) == 1
     assert pow(h2, order, N) == 1
-
 
     g, V = h2, h1
     assert g % p == 1 and g % q == 2
@@ -167,50 +185,74 @@ def _gen_malicious_params():
     template_modified = modify_template(template, [SALT, N, g, V])
     open("brute.cu", "w").write(template_modified)
 
-    if True:
-        r = work(
-            p_buf=_int_buffer(p),
-            q_buf=_int_buffer(q),
-            p_inv_buf=_int_buffer(inv_p_mod_q),
-            max_r = 2**min(P_LENGTH+10, 64),
-            threads = 256,
-            blocks = 64,
-            kernel_batch_size = 1000,
-        )
-        if r is not None:
-            for rr in range(r,r+1):
-                # x = pow(2, rr, q)
-                x = pow(g, rr, N)
-                c = _hash_ints([SALT, N, g, V, x])
-                print("salt=", SALT)
-                print("N=", N)
-                print("g=", g)
-                print("v=", V)
-                print("x=", x)
-                print("H1=", h1)
-                print("H2=", h2)
-                print("R=", rr)
-                print("C=", c)
-                print("P=", p)
-                print("Q=", q)
-                print("N=", N)
-                print("c % (p - 1)/2 =", c % ((p - 1) // 2))
-        proof_dlog_h2_base_h1 = None
-    else:
-        # forge proof for log(h1, base=h2)
-        g, V = h2, h1
-        time_start = time.time()
-        for r in range(p):
-            c = _hash_ints([SALT, N, g, V, pow(g, r, N)])
-            if c % (p - 1) == 0:
-                break # Found
-            if r%10000==0:
-                print("Speed = ", (r+1)/((time.time()-time_start)*1000), "K/s")
-        print("Speed = ", (r+1)/((time.time()-time_start)*1000), "K/s")
+    r = work(
+        p_buf=_int_buffer(p),
+        q_buf=_int_buffer(q),
+        p_inv_buf=_int_buffer(inv_p_mod_q),
+        args=args,
+        max_r=2**min(P_LENGTH+10, 64),)
+    if r is not None:
+        for rr in range(r,r+1):
+            # x = pow(2, rr, q)
+            x = pow(g, rr, N)
+            c = _hash_ints([SALT, N, g, V, x])
+            print("salt=", SALT)
+            print("N=", N)
+            print("g=", g)
+            print("v=", V)
+            print("x=", x)
+            print("H1=", h1)
+            print("H2=", h2)
+            print("R=", rr)
+            print("C=", c)
+            print("P=", p)
+            print("Q=", q)
+            print("N=", N)
+            print("c % (p - 1)/2 =", c % ((p - 1) // 2))
+    proof_dlog_h2_base_h1 = None
+    # else:
+    #     # forge proof for log(h1, base=h2)
+    #     g, V = h2, h1
+    #     time_start = time.time()
+    #     for r in range(p):
+    #         c = _hash_ints([SALT, N, g, V, pow(g, r, N)])
+    #         if c % (p - 1) == 0:
+    #             break # Found
+    #         if r%10000==0:
+    #             print("Speed = ", (r+1)/((time.time()-time_start)*1000), "K/s")
+    #     print("Speed = ", (r+1)/((time.time()-time_start)*1000), "K/s")
     y = (r - c // (p - 1)) % phiN
     proof_dlog_h1_base_h2 = (y, c)
 
     return p, q, h1, h2, proof_dlog_h2_base_h1, proof_dlog_h1_base_h2
 
 
-p, q, h1, h2, proof_dlog_h2_base_h1, proof_dlog_h1_base_h2 = _gen_malicious_params()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--p', type=int, required=False, help="Optional P value (random if not provided)")
+    parser.add_argument('--q', type=int, required=False, help="Optional Q value (random if not provided)")
+    parser.add_argument('--Nbit', type=int, required=False, help="Size of N (defaults 2048)", default=2048)
+    parser.add_argument('--Pbit', type=int, required=False, help="Size of P")
+    parser.add_argument('--Qbit', type=int, required=False, help="Size of Q")
+    parser.add_argument('--threads', type=int, required=False, help="Number of threads per block (block dim), default to 256", default=256)
+    parser.add_argument('--blocks', type=int, required=False, help="Number of blocks per grid (grid dim), default to 64", default=64)
+    parser.add_argument('--kernels', type=int, required=False, help="Number of concurrent kernels to used, default to 1", default=1)
+    parser.add_argument('--batch', type=int, required=False, help="Loop count of each threads, default 1000", default=1000)
+    args = parser.parse_args()
+
+    if args.p and args.q:
+        args.Nbit = len(bin(args.p)) - 2 + len(bin(args.q)) - 2
+    if args.p:
+        args.Pbit = len(bin(args.p)) - 2
+    if args.q:
+        args.Qbit = len(bin(args.p)) - 2
+
+    if all(map(lambda x: x is None, [args.p, args.q, args.Pbit])):
+        print("atleast Pbit is required, if no p and q are given")
+        exit(0)
+
+    p, q, h1, h2, proof_dlog_h2_base_h1, proof_dlog_h1_base_h2 = _gen_malicious_params(args)
+
+if __name__ == "__main__":
+    main()
